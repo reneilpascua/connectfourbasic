7533d727f40e7c941762d76a4ad1da363160e8d2
Reneil Pascua <rpascua2@my.bcit.ca>
2018-10-30 04:39:57 -0700

    improved game QOL

--- src/Connect4GUI.java
+++ src/Connect4GUI.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -14,17 +15,20 @@ import javafx.stage.Stage;
 
 public class Connect4GUI extends Application {
 
+    boolean gameover = false;
     GameState gs = new GameState(6,7);
     GridPane board = new GridPane();
     Scene scene;
+    Scene winscene;
     final private int cellSz = 50;
     final int buffer = 5;
     int turncounter = 0;
     List<Circle> slots = new ArrayList<Circle>();
     
-    List<Button> here = new ArrayList<Button>();
+    
     
     public void start(Stage stg) throws Exception {
+        
         scene = new Scene(board, cellSz*7 + buffer*8, cellSz*6 + buffer*7);
         
         for (int r=0; r<6; r++) {
@@ -45,24 +49,28 @@ public class Connect4GUI extends Application {
     }
     
     public void dropPiece(MouseEvent m) {
-        int x = (int) m.getX();
-        /*
-         * double y = m.getY();
-         * System.out.printf("x: %.2f\ty:%.2f\n",x,y);
-         */
-        int selColumn = x/(cellSz+buffer);
-        turncounter++;
-        int player = (turncounter%2 !=0)?1:2;
-        // record move on the abstract board
-        int dropPlace = gs.dropPiece(selColumn, player);
-        
-        slots.get(7*dropPlace + selColumn).setFill((player==1)?Color.RED:Color.YELLOW);
-        
-        //check if win
-        if (gs.checkWin(player)) {
-            System.out.println("Player "+player+" has won.");
+        if (!gameover) {
+            int x = (int) m.getX();
+            /*
+             * double y = m.getY();
+             * System.out.printf("x: %.2f\ty:%.2f\n",x,y);
+             */
+            int selColumn = x/(cellSz+buffer);
+            turncounter++;
+            int player = (turncounter%2 !=0)?1:2;
+            // record move on the abstract board
+            int dropPlace = gs.dropPiece(selColumn, player);
+            
+            slots.get(7*dropPlace + selColumn).setFill((player==1)?Color.RED:Color.YELLOW);
+            
+            //check if win
+            if (gs.checkWin(player)) {
+                Label winner = new Label("Player "+player+" has won!");
+                board.add(winner,2,3,3,1);
+                winner.setAlignment(Pos.CENTER);
+                gameover = true;
+            }
         }
-        
     }
     
     public static void main(String[] args) {
